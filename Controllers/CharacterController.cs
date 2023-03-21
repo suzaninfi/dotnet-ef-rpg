932bcc4108c37a20998d9fabe69808a7839e83d8
sdste <suzan@infi.nl>
2023-03-21 08:46:15 +0100

    Add first controller and get method

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_ef_rpg.Controllers;
+
+[ApiController] // attribute, indicates that this is used to serve HTTP API responses
+[Route("api/[controller]")] // attribute, means this controller can be found at api/Character
+public class CharacterController: ControllerBase
+{
+    private static Character knight = new Character();
+
+    [HttpGet]
+    public ActionResult<Character> Get()
+    {
+        return Ok(knight); // sends status code 200 OK
+        
+        // Other options (among others):
+        // BadRequest(knight) // 400 status
+        // NotFound(knight) // 404 status
+    }
+}
--- Models/RpgClass.cs
+++ Models/RpgClass.cs
@@ -1,5 +1,8 @@
-﻿namespace dotnet_ef_rpg.Models;
+﻿using System.Text.Json.Serialization;
 
+namespace dotnet_ef_rpg.Models;
+
+[JsonConverter(typeof(JsonStringEnumConverter))] // converts enum to and from strings, so that swagger puts the names and not the numbers in the schemas
 public enum RpgClass
 {
     Knight = 1,
