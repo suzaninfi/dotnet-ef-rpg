95abadca72848930c1ac242bbc4aef607df9a73f
sdste <suzan@infi.nl>
2023-03-21 08:55:50 +0100

    Add GetAll endpoint

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -6,15 +6,22 @@ namespace dotnet_ef_rpg.Controllers;
 [Route("api/[controller]")] // attribute, means this controller can be found at api/Character
 public class CharacterController: ControllerBase
 {
-    private static Character knight = new Character();
+    private static List<Character> characters = new List<Character> {new Character(), new Character {Name = "Sam"}};
 
-    [HttpGet]
-    public ActionResult<Character> Get()
+    [HttpGet("GetAll")] // Short for [HttpGet] and [Route("GetAll")]
+    public ActionResult<List<Character>> Get()
     {
-        return Ok(knight); // sends status code 200 OK
+        return Ok(characters); // sends status code 200 OK
         
         // Other options (among others):
         // BadRequest(knight) // 400 status
         // NotFound(knight) // 404 status
+    }
+    
+    [HttpGet]
+    public ActionResult<Character> GetSingle()
+    {
+        return Ok(characters[0]);
+        
     }
 }
