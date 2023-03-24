6ec6ceaab13100564573684b5811611a43c93df8
sdste <suzan@infi.nl>
2023-03-24 08:33:30 +0100

    Resolve nullable issue in GetCharacterById

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -2,7 +2,8 @@
 
 public class CharacterService : ICharacterService
 {
-    private static readonly List<Character> characters = new() { new(), new() { Name = "Sam", Id = 1 } };
+    private static readonly List<Character> characters = new()
+        { new Character(), new Character { Name = "Sam", Id = 1 } };
 
     public List<Character> AddCharacter(Character newCharacter)
     {
@@ -17,6 +18,12 @@ public class CharacterService : ICharacterService
 
     public Character GetCharacterById(int id)
     {
-        return characters.FirstOrDefault(character => character.Id == id); // returns first where id matches
+        var character = characters.FirstOrDefault(character => character.Id == id); // returns first where id matches
+        if (character is not null)
+        {
+            return character;
+        }
+
+        throw new Exception("Character not found");
     }
 }
