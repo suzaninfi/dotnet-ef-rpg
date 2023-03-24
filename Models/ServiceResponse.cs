e2ba2d5abc2dbcb529d5b4b6cd928d121ecbe42a
sdste <suzan@infi.nl>
2023-03-24 09:01:29 +0100

    Add ServiceResponse wrapper

---
+++ Models/ServiceResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet_ef_rpg.Models;
+
+public class ServiceResponse<T>
+{
+    public T? Data { get; set; }
+    public bool Success { get; set; } = true;
+    public string Message { get; set; } = string.Empty;
+}
--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -5,25 +5,20 @@ public class CharacterService : ICharacterService
     private static readonly List<Character> characters = new()
         { new Character(), new Character { Name = "Sam", Id = 1 } };
 
-    public async Task<List<Character>> AddCharacter(Character newCharacter)
+    public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
     {
         characters.Add(newCharacter);
-        return characters;
+        return new ServiceResponse<List<Character>> { Data = characters };
     }
 
-    public async Task<List<Character>> GetAllCharacters()
+    public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
     {
-        return characters;
+        return new ServiceResponse<List<Character>> { Data = characters };
     }
 
-    public async Task<Character> GetCharacterById(int id)
+    public async Task<ServiceResponse<Character>> GetCharacterById(int id)
     {
         var character = characters.FirstOrDefault(character => character.Id == id); // returns first where id matches
-        if (character is not null)
-        {
-            return character;
-        }
-
-        throw new Exception("Character not found");
+        return new ServiceResponse<Character> { Data = character };
     }
 }
